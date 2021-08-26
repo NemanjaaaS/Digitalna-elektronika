45dee16d3770fea2174365c2cb94255cbefa1abd
NemanjaaaS <66557612+NemanjaaaS@users.noreply.github.com>
2021-08-26 14:49:25 +0200

    Add files via upload

---
+++ Igrica 2048/colors.py
@@ -0,0 +1,50 @@
+GRID_COLOR = "#d9d9d9"
+EMPTY_CELL_COLOR = "#eae6ff"
+SCORE_LABEL_FONT = ("Verdana", 20)
+SCORE_FONT = ("Helvetica", 32, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#1a53ff"
+LOSER_BG = "#ff5c33"
+
+CELL_COLORS = {
+    2: "#cce6ff",
+    4: "#99ccff",
+    8: "#66b3ff",
+    16: "#3399ff",
+    32: "#0080ff",
+    64: "#0066cc",
+    128: "#004d99",
+    256: "#003366",
+    512: "#00264d",
+    1024: "#001a33",
+    2048: "#000000"
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#ffffff",
+    4: "#ffffff",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
+
+CELL_NUMBER_FONTS = {
+    2: ("Helvetica", 55, "bold"),
+    4: ("Helvetica", 55, "bold"),
+    8: ("Helvetica", 55, "bold"),
+    16: ("Helvetica", 50, "bold"),
+    32: ("Helvetica", 50, "bold"),
+    64: ("Helvetica", 50, "bold"),
+    128: ("Helvetica", 45, "bold"),
+    256: ("Helvetica", 45, "bold"),
+    512: ("Helvetica", 45, "bold"),
+    1024: ("Helvetica", 40, "bold"),
+    2048: ("Helvetica", 40, "bold")
+}
